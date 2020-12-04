8c119effe0ae72f9ea9e3721f17043e78d36d4ae
Forrest <zyyu1990@gmail.com>
2020-12-04 14:39:20 +0800

    fix

--- Sources/TorusUtils/TorusUtils.swift
+++ Sources/TorusUtils/TorusUtils.swift
@@ -39,16 +39,15 @@ public class TorusUtils{
                     // Do keylookup again
                     return self.keyLookup(endpoints: endpoints, verifier: verifier, verifierId: verifierId)
                 }.then{ data -> Promise<[String: String]> in
-                    
+                    if let error = lookupData["err"] {
+                        throw error
+                    }
                     return Promise<[String: String]>.value(data)
                 }
             }else{
                 return Promise<[String: String]>.value(lookupData)
             }
         }.then{ data in
-            if let error = lookupData["err"] {
-                throw error
-            }
             return self.getMetadata(dictionary: ["pub_key_X":data["pub_key_X"]!, "pub_key_Y": data["pub_key_Y"]!]).map{ ($0, data) } // Tuple
         }.done{ nonce, data in
             var newData = data
